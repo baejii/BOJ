08110c58fccda42355bf17f18fc946102bcd8a9b
baejii <jiyn54311@gmail.com>
2024-04-24 00:45:03 +0900

    [Add] Step 3 problem solving

---
+++ Step3_iteration/boj_11021.js
@@ -0,0 +1,14 @@
+/*
+ * https://www.acmicpc.net/problem/11021
+ * A+B - 7
+ * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ */
+
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let result = "";
+for (let i = 1; i <= input[0]; i++) {
+    const [a, b] = input[i].split(' ').map(Number);
+    result = a+b;
+    console.log('Case #' + i + ': ' + result);
+}
