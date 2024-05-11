9d20bf872d9ad825ded847d3898d31903620b7c4
baejii <jiyn54311@gmail.com>
2024-05-11 21:48:40 +0900

    [Add] Step 13 problem solving

---
+++ Step13_sort/boj_2751.js
@@ -0,0 +1,13 @@
+/*
+ * https://www.acmicpc.net/problem/2751
+ * 수 정렬하기2
+ * N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+ */
+
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
+const arr = [];
+for(let i = 1; i <= input.length; i++) {
+    arr.push(input[i]);
+}
+const result = arr.sort((x, y) => x - y);
+console.log(result.join('\n'));
