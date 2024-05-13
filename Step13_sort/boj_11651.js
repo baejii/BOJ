709626208a048b4c99e4ab7130903a68c2c6fef6
baejii <jiyn54311@gmail.com>
2024-05-13 17:08:17 +0900

    [Update] Step 13 problem solving

--- Step13_sort/boj_11651.js
+++ Step13_sort/boj_11651.js
@@ -4,7 +4,7 @@
  * 2차원 평면 위의 점 N개가 주어진다. 좌표를 y좌표가 증가하는 순으로, y좌표가 같으면 x좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
  */
 
-const input = require('fs').readFileSync('example.txt').toString().trim().split('\n');
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const n = parseInt(input[0]);
 
